a14544122da6ecf6d4e03435dc36a25a865699e7
Sencefs <100245563+Sencefs@users.noreply.github.com>
2022-06-09 10:59:08 -0400

    Add files via upload

---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,29 @@
+//Function to generate markdown for readme
+const generateMarkdown = data => {
+  return `# ${data.title}
+  ![Github license](http://img.shields.io/badge/license-${data.license}-blue.svg)
+  ## Description 
+  ${data.description}
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  ## Installation 
+  ${data.install}
+  ## Usage 
+  ${data.usage}
+  ## License 
+  This project is license under ${data.license}
+  ## Contributing 
+  ${data.contributors}
+  ## Tests
+  ${data.test}
+  ## Questions
+  If you have any questions about this projects, please contact me directly at ${data.email}. You can view more of my projects at https://github.com/${data.username}.
+`;
+}
+//Utilized for importing Markdown in index 
+module.exports = generateMarkdown;
